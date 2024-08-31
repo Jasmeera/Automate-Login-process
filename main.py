eead5d5a781e01403d377055cd3abce50bc05431
Jasmeera <jasmeerajanee1@gmail.com>
2024-08-31 03:22:29 +0000

    Login and Click

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
 def get_driver():
   #set options to make browsing easier
   options = webdriver.ChromeOptions()
@@ -10,12 +13,16 @@ def get_driver():
   options.add_argument("disable-blink-features=AutomationControlled")
   
   driver = webdriver.Chrome(options=options)
-  driver.get("http://automated.pythonanywhere.com/")
+  driver.get("http://automated.pythonanywhere.com/login/")
   return driver
 
 def main():
   driver = get_driver()
-  element = driver.find_element(by="xpath",value="/html/body/div[1]/div/h1[1]")
-  return element.text
-
+  driver.find_element(by="id",value="id_username").send_keys("automated")
+#takda return, it will not return anything
+  time.sleep(2) #time gap 2 sec
+  driver.find_element(by="id",value="id_password").send_keys("automatedautomated"+ Keys.RETURN)
+  time.sleep(2) 
+  driver.find_element(by="xpath", value = "/html/body/nav/div/a").click()
+  print (driver.current_url) #ni print url yang kita nak masuk
 print(main())
