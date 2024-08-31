8d6e10435abc71816b4eb306afe39b81fb0d8ed6
Jasmeera <jasmeerajanee1@gmail.com>
2024-08-31 03:31:15 +0000

    scrape dynamic value at login

--- main.py
+++ main.py
@@ -16,13 +16,30 @@ def get_driver():
   driver.get("http://automated.pythonanywhere.com/login/")
   return driver
 
+#function untuk ambik no sahaja
+def clean_text(text):
+  """"Extract only the temperature from text"""
+  output = float(text.split(": ")[1])
+  return output
+  
 def main():
   driver = get_driver()
+
+  #Find and fill username and password
   driver.find_element(by="id",value="id_username").send_keys("automated")
 #takda return, it will not return anything
   time.sleep(2) #time gap 2 sec
   driver.find_element(by="id",value="id_password").send_keys("automatedautomated"+ Keys.RETURN)
   time.sleep(2) 
+
+  #Click on Home Link and wait 2 sec
   driver.find_element(by="xpath", value = "/html/body/nav/div/a").click()
   print (driver.current_url) #ni print url yang kita nak masuk
+  time.sleep(2) #wait for 2 seconds untuk tunggu halaman selesai loading
+  
+  #scrape temperature value
+  element = driver.find_element(by="xpath",value="/html/body/div[1]/div/h1[2]")
+  return clean_text(element.text)
+
+  
 print(main())
